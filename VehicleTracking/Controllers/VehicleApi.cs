3ae9470a1c3f2058c7a40987511b2ae138b1e0f4
Eefan <jayyjayyukcs@hotmail.co.uk>
2022-01-08 14:00:52 +0000

    created base models and apis

---
+++ VehicleTracking/Controllers/VehicleApi.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VehicleTracking.Controllers
+{
+    public class VehicleApi : Controller
+    {
+        //add auth
+        [HttpPost]
+        [Route("api/vehicle")]
+        public virtual JsonResult CreateVehicle()
+        {
+            //register vehicle
+
+            //register initial location
+
+            return new JsonResult(Ok());
+        }
+
+        [HttpPut]
+        [Route("api/vehicle")]
+        public virtual JsonResult ReplaceVehicle(Guid id)
+        {
+            //register vehicle
+
+            //register initial location
+
+            return new JsonResult(Ok());
+        }
+
+        [HttpPost]
+        [Route("api/vehicle")]
+        public virtual JsonResult UpdateVehicle(Guid id)
+        {
+            //register vehicle
+
+            //register initial location
+
+            return new JsonResult(Ok());
+        }
+
+        [HttpGet]
+        [Route("api/vehicle/{id}")]
+        public virtual JsonResult GetVehicle()
+        {
+            //get vehicle
+            return new JsonResult(Ok());
+        }
+    }
+}
---
+++ VehicleTracking/Models/Location.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VehicleTracking.Models
+{
+    public class Location
+    {
+        public double Lat { get; set; }
+        public double Long { get; set; }
+    }
+}
