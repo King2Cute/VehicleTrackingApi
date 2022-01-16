9a7eb89f37955a301add8c5e37271a50bf1fba79
Eefan <jayyjayyukcs@hotmail.co.uk>
2022-01-16 17:46:33 +0000

    Update BaseLocation.cs
    removed json ignore from name

--- VehicleTracking.Models/BaseLocation.cs
+++ VehicleTracking.Models/BaseLocation.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Text.Json.Serialization;
 
 namespace VehicleTracking.Models
 {
     public class BaseLocation : Location
     {
         public DateTime Time { get; set; }
-        [JsonIgnore]
         public string Name { get; set; }
     }
 }
